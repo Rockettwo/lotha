e811c8ed261a790fdcb1251190dce68e911209a4
Rockettwo <felix.b@online.de>
2019-08-04 23:17:02 +0200

    fix tests and better implementation of Credentials hiding

---
+++ lib/ApiCredentials.dart
@@ -0,0 +1,9 @@
+// Update this file accordung your account. It is ignored by git  (git update-index --skip-worktree **/lib/ApiCredentials.dart)
+
+class APICredentials {
+
+  // Oxfords
+  final appIdOxford  = "<your appId>";
+  final appKeyOxford = "<your appKey>";
+
+}
